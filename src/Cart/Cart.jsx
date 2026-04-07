120d9347b2bb3011d998b6ced73231bf6f926472
fahmidaafrozmoni2928-droid <fahmidaafrozmoni2928@gmail.com>
2026-04-07 12:10:52 +0600

    added toggle

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -2,22 +2,35 @@ import React from "react";
 
 const Cart = ({carts, setCarts}) => {
     return (
-        <div>
-            <h1 className="text-4xl font-bold">Your Cart</h1>
+        <div className="border mx-20 my-20 py-10 border-gray-300 rounded-lg">
+            <h1 className="text-6xl font-bold container mx-auto pl-22">Your Cart</h1>
+            <div className="space-y-4">
             {
-                carts.map(cart => 
-                    <div>
-                        <div className="flex">
+                carts.map(cart =>
+                    
+                    <div className="">
+                        <div className="flex gap-6 border border-[#A2DDFD]/10 mx-50 py-4 bg-[#A2DDFD]/10 rounded-lg">
                         <a className="text-4xl">{cart.icon}</a>
 
                         <div className="">
-                            <h1 className="text-2xl font-bold">{cart.name}</h1>
+                            <h1 className="text-xl font-bold">{cart.name}</h1>
                             <p className="text-gray-600">${cart.price}</p>
                         </div>
+                        <button className="btn text-red-400 justify-end ml-150">Remove</button>
                         </div>
+
+                       
+
                     </div>
+                    
                 )
             }
+            </div>
+            <div className="flex justify-between">
+                            <h3>Total</h3>
+                            <p>$0</p>
+                        </div>
+            
         </div>
     )
 }
